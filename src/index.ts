d2986ae84831d1e4d7d9831b6975c3ec8461f0ed
ehsanpaknahad <ehsanpaknahad@yahoo.com>
2024-07-24 11:41:54 +0330

    finish

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { CharactersCollection } from "./CharactersCollection"
+import { NumbersCollections } from "./NumbersCollection"
+import { Sorter } from "./Sorter"
+ 
+
+const charactersCollection = new CharactersCollection('mynameisehsanlll')
+charactersCollection.sort()
+console.log(charactersCollection.data)
+
+
+// const numbersCollection = new NumbersCollections([-9,1211,5,215,164,8])
+// numbersCollection.sort()
+// console.log(numbersCollection.data)
